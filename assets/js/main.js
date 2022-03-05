fed37d071335588a60fa4c9b6eaaadfbffe7dbf1
Prateek Varshney <varshneyprateek1993@gmail.com>
2022-03-05 19:15:00 +0530

    Styling issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,10 @@ var weddingDate = new Date('04-22-2022');
 function initCountDown() {
 
     let diffTime = Math.abs(weddingDate - new Date());
-    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    let diffHours = Math.ceil(diffTime / (1000 * 60 * 60)) % 24;
-    let diffMins = Math.ceil(diffTime / (1000 * 60)) % 60;
-    let diffSeconds = Math.ceil(diffTime / (1000)) % 60;
+    let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    let diffHours = Math.floor((diffTime / (1000 * 60 * 60)) % 24);
+    let diffMins = Math.floor((diffTime / (1000 * 60)) % 60);
+    let diffSeconds = Math.floor((diffTime / (1000))  % 60);
     document.getElementById("std-seconds").innerHTML = diffSeconds;
     document.getElementById("std-minutes").innerHTML = diffMins;
     document.getElementById("std-hours").innerHTML = diffHours;
